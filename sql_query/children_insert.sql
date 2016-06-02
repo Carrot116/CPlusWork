4bb5ad08b56e22eebf857ad9779bc8c7fa7ec22b
Carrot <Carrot@MacMini.local>
2016-06-02 18:54:21 +0800

    sql

---
+++ sql_query/children_insert.sql
@@ -0,0 +1,8 @@
+insert into children (fname, age) values ('july', 4);
+insert into children (fname, age) values ('sum', 3);
+insert into children (fname, age) values ('jack', 2);
+insert into children (fname, age) values ('kobe', 1);
+insert into children (fname, age) values ('lusy', 6);
+insert into children (fname, age) values ('bili', 5);
+insert into children (fname, age) values ('lily', 7);
+insert into children (fname, age) values ('jason', 3);
---
+++ sql_query/fname_create.sql
@@ -0,0 +1,3 @@
+CREATE table children(childno int(11) auto_increment not null primary key,
+fname varchar(30),
+age int);
