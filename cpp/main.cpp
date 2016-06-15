d9670e51d14f7695f3367e592ab3ec5cba50c6d5
Carrot <Carrot@MacMini.local>
2016-06-15 14:57:32 +0800

    ss

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -30,6 +30,22 @@ void Base::print() {
     cout << num << name << endl;
 }
 
+int testEggs(){
+    int count = 0;
+    bool bFind = false;
+    do {
+        count++;
+        if (count % 2 == 1 && count % 3 == 0 && count % 4 == 1 && count % 5 == 4 &&
+            count % 6 == 3 && count % 7 == 5 && count % 8 == 1 && count % 9 == 0){
+            bFind = true;
+        }
+    } while (bFind == false);
+    return count;
+};
+
+int test(){
+    return 1;
+}
 
 int main() {
     cout << "Hello, World!" << endl;
@@ -42,6 +58,8 @@ int main() {
 //    testMenu2();
 
 //    connect2mysql();
-    testWrapper();
+//    testWrapper();
+
+    cout << "Eggs: " << testEggs() << endl;
     return 0;
 }
